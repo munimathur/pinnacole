8a6f283bb4c065cba0eedfc9d76abe3fe013ddb2
munimathur <40835425+munimathur@users.noreply.github.com>
2019-03-26 17:35:39 +0530

    Updated Code Communities
    Updated One

--- src/com/qa/pages2/communities.java
+++ src/com/qa/pages2/communities.java
@@ -1,7 +1,9 @@
 package com.qa.pages2;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -27,18 +29,54 @@ public class communities extends TestBase
 		LoginMyPage();
 
 	}
+	
 
-	@Test(priority=1)
+	@Test(description="To Verify the Communities icon is Showing ",priority=1)
 	public void verifyCommunity() throws Exception
 	{
-		Thread.sleep(5000);
-		WebElement element=driver.findElement(By.xpath("//span[contains(text(),'Communities')]"));
-				if(element.isDisplayed())
-				{
-				System.out.println("Image displayed");
-				}else{
-				System.out.println("Image not displayed");
-				}
+		boolean element=driver.findElement(By.xpath("//li[@id='headtools']//span[contains(text(),'Tools')]")).isDisplayed();
+		Assert.assertTrue(element, "Tools");	
 		
 	}
+	
+	@Test(description="To Verify the Popular Communities icon is clicking ",priority=2)
+	public void clickCommunity()
+	{
+		WebElement element=driver.findElement(By.xpath("//li[@id='headtools']//span[contains(text(),'Tools')]"));
+		element.click();
+	}
+	
+		
+	@Test(description="To Verify the Popular Communities icon is clicking ",priority=3)
+	public void searchCommunity()
+	{
+		WebElement element=driver.findElement(By.xpath("//li[@id='headtools']//span[contains(text(),'Tools')]"));
+		element.click();
+		
+		WebElement search=driver.findElement(By.xpath("//li[@id='headtools']//span[contains(text(),'Tools')]"));
+		search.sendKeys("Digital");
+		search.submit();
+	}
+	
+	
+	@Test(description="To Verify the Popular Communities icon is cliking",priority=4)
+	public void verifyPopularCommunities()
+	{
+		WebElement element=driver.findElement(By.xpath("//li[@id='headtools']//span[contains(text(),'Tools')]"));
+		element.click();
+		
+		boolean element1=driver.findElement(By.xpath("//a[contains(text(),'Popular Communities')]")).isDisplayed();
+		Assert.assertTrue(element1, "Popular Communities");	
+	}
+	
+	@Test(description="To Verify the All Communities icon is clicking",priority=5)
+	public void verifyAllCommunities()
+	{
+		WebElement element=driver.findElement(By.xpath("//li[@id='headtools']//span[contains(text(),'Tools')]"));
+		element.click();		
+		
+		boolean element1=driver.findElement(By.xpath("//li[@id='leftallcommunities']//a[1]")).isDisplayed();
+		Assert.assertTrue(element1, "All Communities");	
+	}
+	
 }
